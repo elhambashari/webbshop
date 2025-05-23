5822ee46ba5b4a4dce796aa131522beae1da6a24
Elham Bashari <elhambashari84@gmail.com>
2025-05-23 10:27:19 +0200

    npm test iscartitem red

--- src/test/validation.test.js
+++ src/test/validation.test.js
@@ -1,6 +1,6 @@
 
 
-import{isProduct} from '../validation.js'
+import{isProduct,isCartItem} from '../validation.js'
 
 describe('isProduct',()=>{
 	test('om det finns en riktig produkt visa true',()=>{
@@ -11,4 +11,14 @@ describe('isProduct',()=>{
 
 test('om finns inte produkt visa false',()=>{
 	expect(isProduct({id:'A6',name:'byxor'})).toBe(false)
+})
+
+describe('isCartItem', () => {
+  test('om det finns item i cart visa true', () => {
+    expect(isCartItem({ id: 'p1', name: 'T-shirt', price: 100, quantity: 2 })).toBe(true)
+  })
+
+  test('om det finns ingen item visa false', () => {
+    expect(isCartItem({ id: 'p1', name: 'T-shirt', price: 100 })).toBe(false)
+  })
 })
