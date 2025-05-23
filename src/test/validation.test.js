cac6871b276248e7c71aa2190a98c44f1cb389b8
Elham Bashari <elhambashari84@gmail.com>
2025-05-23 10:35:21 +0200

    npm test iscartitem green

--- src/test/validation.test.js
+++ src/test/validation.test.js
@@ -4,10 +4,10 @@ import{isProduct,isCartItem} from '../validation.js'
 
 describe('isProduct',()=>{
 	test('om det finns en riktig produkt visa true',()=>{
-
 		expect(isProduct({id:'A6',name:'byxor', price:200})).toBe(true)
 	})
 })
+
 
 test('om finns inte produkt visa false',()=>{
 	expect(isProduct({id:'A6',name:'byxor'})).toBe(false)
@@ -15,10 +15,10 @@ test('om finns inte produkt visa false',()=>{
 
 describe('isCartItem', () => {
   test('om det finns item i cart visa true', () => {
-    expect(isCartItem({ id: 'p1', name: 'T-shirt', price: 100, quantity: 2 })).toBe(true)
+    expect(isCartItem({ id: 'A6', name: 'byxor', price: 200, quantity: 2 })).toBe(true)
   })
 
   test('om det finns ingen item visa false', () => {
-    expect(isCartItem({ id: 'p1', name: 'T-shirt', price: 100 })).toBe(false)
+    expect(isCartItem({ id: 'A6', name: 'byxor', price: 200 })).toBe(false)
   })
 })
--- src/validation.js
+++ src/validation.js
@@ -3,3 +3,6 @@ export function isProduct(obj) {
   return obj && typeof obj.id === 'string' && typeof obj.name === 'string' && typeof obj.price === 'number';
 }
 
+export function isCartItem(obj) {
+  return isProduct(obj) && typeof obj.quantity === 'number'
+}
