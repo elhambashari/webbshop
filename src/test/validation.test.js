9b1d33a133f901b2959c1b352b2807b7ead21a97
Elham Bashari <elhambashari84@gmail.com>
2025-05-23 09:38:37 +0200

    npm test (isproduct) red

--- src/test/validation.test.js
+++ src/test/validation.test.js
@@ -1,1 +1,10 @@
 
+
+import{isProduct} from '../validation.js'
+
+describe('isProduct',()=>{
+	test('om det finns en riktig produkt visa true',()=>{
+
+		expect({id:'A6',title:'byxor', price:200}).toBe(true)
+	})
+})
